d53d2f580d02a2d2a967ef009769f72222b351e0
nazman <nazmanmuhammad.business@gmail.com>
2022-05-28 00:10:14 +0700

    mytwo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,7 +10,7 @@
     <meta name="keywords" content="au theme template">
 
     <!-- Title Page-->
-    <title>Dashboard</title>
+    <title>Dash111board</title>
 
     <!-- Fontfaces CSS-->
     <link href="{{asset('admin/css/font-face.css')}}" rel="stylesheet" media="all">
